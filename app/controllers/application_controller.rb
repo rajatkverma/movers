9284f3687dfa5c916ed6034944676c68d9c9f115
akshayn138888 <akshayn@sfu.ca>
2017-08-19 16:06:03 -0700

    about us and getestimate page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+
+  def show 
+
+  end
+
+
+  def edit 
+
+  end
+
+
+
+  def delete 
+
+  end 
+
+  
 end
